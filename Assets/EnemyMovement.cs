e4d9fd3becbe2ee97ae7e1e9667f4bc05c0234e8
valeriomathew05-commits <valeriomathew05@gmail.com>
2026-07-23 08:02:03 +0800

    1st update for level 1 july 23

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -11,16 +11,23 @@ public class EnemyMovement : MonoBehaviour
 
     [SerializeField] private int startDirection = 1;
 
+    [SerializeField] private bool stayOnLedges = true;
+
     private int currentDirection;
 
     private float halfWidth;
 
+    private float halfHeight;
+
     private Vector2 movement;
+
+    private bool isGrounded;
 
     // Start is called before the first frame update
     private void Start()
     {
         halfWidth = spriteRenderer.bounds.extents.x;
+        halfHeight = spriteRenderer.bounds.extents.y;
         currentDirection = startDirection;
         spriteRenderer.flipX = startDirection == 1 ? false : true;
     }
@@ -34,17 +41,61 @@ public class EnemyMovement : MonoBehaviour
         SetDirection();
     }
 
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+
     private void SetDirection()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && rigidbody.velocity.x > 0)
+        if (!isGrounded) return;
+
+        Vector2 rightPos = transform.position;
+        Vector2 leftPos = transform.position;
+        rightPos.x += halfWidth;
+        leftPos.y -= halfHeight;
+
+        if (rigidbody.velocity.x > 0)
         {
-            currentDirection *= -1;
-            spriteRenderer.flipX = true;
+            if (Physics2D.Raycast(transform.position, Vector2.right, halfWidth + 0.1f, LayerMask.GetMask("Ground")))
+        {
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }
+
+            else if (stayOnLedges && !Physics2D.Raycast(rightPos, Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Ground")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = true;
+            }
         }
-        else if (Physics2D.Raycast(transform.position, Vector2.left, halfWidth + 0.1f, LayerMask.GetMask("Ground")) && rigidbody.velocity.x < 0)
+
+        else if (rigidbody.velocity.x < 0)
         {
-            currentDirection *= -1;
-            spriteRenderer.flipX = false;
+            if (Physics2D.Raycast(transform.position, Vector2.left, halfWidth + 0.1f, LayerMask.GetMask("Ground")))
+        {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+            }
+
+            else if (stayOnLedges && !Physics2D.Raycast(leftPos, Vector2.down, halfHeight + 0.1f, LayerMask.GetMask("Ground")))
+            {
+                currentDirection *= -1;
+                spriteRenderer.flipX = false;
+            }
         }
 
         Debug.DrawRay(transform.position, Vector2.right * (halfWidth + 0.1f), Color.red);
